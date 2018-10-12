d10200dc22b4f91976076008158b04187aa02ff1
Александр Гоцулов <agotsulov@gmail.com>
2018-10-12 18:33:22 +0600

    6

--- workshops/Workshop1/src/main/java/it/sevenbits/workshop/Main.java
+++ workshops/Workshop1/src/main/java/it/sevenbits/workshop/Main.java
@@ -3,11 +3,10 @@ package it.sevenbits.workshop;
 public class Main {
 
     public static void main(String[] args) {
-        Matrix matrix = new Matrix(10, 8);
+        Matrix matrix = new Matrix(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
         System.out.println(matrix.toString());
 
         matrix.inverseMatrix();
         System.out.println(matrix.toString());
-
     }
 }
